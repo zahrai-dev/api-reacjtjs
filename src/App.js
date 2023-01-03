f9ae13c58db60ef8e15585fce49e149c3468c397
zahrai-dev <zahrai.dev@gmail.com>
2023-01-03 16:09:27 +0700

    first-api-reactjs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React,{ Component }from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class  App extends Component{
+
+constructor(props){
+  super(props)
+  this.state = {
+    items: [],
+    isLoading: true
+  }
+}
+
+componentDidMount(){
+  fetch("https://jsonplaceholder.typicode.com/users")
+  .then (response => response.json())
+  .then (data => this.setState({items : data, isLoading:false }))
+}
+
+  render()  {
+    
+    const { items,isLoading } = this.state
+
+    if(isLoading){
+      return <p> Sabar, sedang proses...</p>
+    }
+    return (
+    <div>
+      <ul>
+        { items.map ((item, index) =>
+          <li key={index}> {item.name} </li>) }
+      </ul>
     </div>
   );
+}
+
 }
 
 export default App;
